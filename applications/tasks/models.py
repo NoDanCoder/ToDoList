00859dfba392f23fbfed4e031c973de909e49d7e
Daniel Cumaco <dancoder@protonmail.com>
2020-11-28 09:56:46 -0500

    [Add] tasks app model created and migrated

--- applications/tasks/models.py
+++ applications/tasks/models.py
@@ -1,4 +1,14 @@
+""" tasks ORM module """
+
+# Django
 from django.db import models
 
 # Create your models here.
+class TasksModel(models.Model):
+    """ tasks db model """
+
+    title = models.CharField(max_length=32, blank=False, unique=True)
+    description = models.TextField(blank=True)
+    estimated_time = models.IntegerField(blank=False)
+    worked_time = models.IntegerField(blank=False)
 
