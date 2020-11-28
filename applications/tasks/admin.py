0e2baa5e0f57798b33b9d9a4ad4c956084db13e3
Daniel Cumaco <dancoder@protonmail.com>
2020-11-28 10:14:17 -0500

    [Add] tasks app model to django-admin panel

--- applications/tasks/admin.py
+++ applications/tasks/admin.py
@@ -1,3 +1,15 @@
+""" tasks app admin module """
+
+# Django
 from django.contrib import admin
 
+# Local
+from .models import TasksModel
+
 # Register your models here.
+@admin.register(TasksModel)
+class TasksAdmin(admin.ModelAdmin):
+    """ tasks admin class """
+
+    # Table columns
+    list_display = ('pk', 'title', 'description', 'estimated_time', 'worked_time')
