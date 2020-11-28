59aaa214a7162d9b6edfa6d0b5e5c1574b1148c1
Daniel Cumaco <dancoder@protonmail.com>
2020-11-28 11:14:11 -0500

    [Add] remaining_time field tasks model, and conect it with template

--- applications/tasks/views.py
+++ applications/tasks/views.py
@@ -1,8 +1,22 @@
 """ tasks views module """
+
+# Django
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
+
+# Local
+from .models import TasksModel
+from .operations import SoapInts
 
 # Create your views here.
 def index(request):
     """ index test module """
-    return render(request, 'index.html')
+
+    if request.method == 'GET':
+
+        context = {
+            'tasks': TasksModel.objects.all()
+        }
+
+        return render(request, 'index.html', context)
