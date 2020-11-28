abf369c5cc732f15cc02dc06a4760d9422b16658
Daniel Cumaco <dancoder@protonmail.com>
2020-11-28 09:56:01 -0500

    [Mod] tasks app names

--- applications/tasks/apps.py
+++ applications/tasks/apps.py
@@ -1,5 +1,10 @@
+""" app entry point module """
+
+# Django
 from django.apps import AppConfig
 
 
 class TasksConfig(AppConfig):
+    """ class label app """
     name = 'tasks'
+    verbose_name = 'Tasks'
